8f1a335a4a4c921592bd2e6c51b0bf53a0a1e05c
David Mansy <david.mansy@me.com>
2013-11-02 13:20:55 +0100

    Implement stringifyJSON for numbers, strings, booleans and arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,43 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to have to write it from scratch:
+var counter = 0;
+
 var stringifyJSON = function (obj) {
-  // your code goes here
+
+  counter++;
+  console.log('COUNTER: ' + counter);
+
+  //Object or Array
+  if (typeof obj === 'object') {
+    //Array
+    if (Array.isArray(obj)) {
+      if (obj.length === 0) {
+        console.log('empty Array ' + '[]');
+        return '[]';
+      } else {
+        var result = '[';
+        obj.forEach(function(value) {
+          if (result != '[') {
+            result += ',';
+          }
+          result += stringifyJSON(value);
+         });
+        console.log('array ' + result + ']');
+        return result + ']';
+      }
+    //Object
+    } else {
+      //TO BE CONTINUED
+    
+    }
+  //No String
+  } else if (typeof obj != 'string') {
+    console.log('no string ' + "" + obj);
+    return "" + obj;
+  //String
+  } else {
+    console.log('string ' + "\"" + obj + "\"");
+    return "\"" + obj + "\"";
+  }
 };
