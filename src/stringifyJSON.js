bcba051e8f4138371c54a5342469e46f9c7630db
David Mansy <david.mansy@icloud.com>
2013-12-28 14:03:56 +0100

    Refactoring

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,78 +2,54 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to have to write it from scratch:
-
 var stringifyJSON = function (obj) {
 
   //Error object that will be thrown if a weird obj is met
   var WeirdObjectError = new Error('Weird');
 
-  //Utility function to stringify an array
-  var stringifyArray = function(obj) {
-    var result = '[';
-    obj.forEach(function(value) {
-      if (result != '[') {
-        result += ',';
-      }
-      //Thx recursion!
-      result += stringify(value);
-     });
-    return result + ']';
-  }
-
-  //Utility function to stringify an object
+  //Utility function to stringify an object and to catch any WeirdObjectError
   var stringifyObject = function(obj) {
-    var result = '{';
-    for (var prop in obj) {
-      if (result != '{') {
-        result += ',';
+    try {
+      var arrayString = [];
+      for (var prop in obj) {
+        var str = stringifyJSON(prop) + ':' + stringifyJSON(obj[prop]);
+        arrayString.push(str);
       }
-      //Thx recursion!
-      result += stringify(prop) + ':' + stringify(obj[prop]);
+      return '{' + arrayString.join(',') + '}';          
+    } catch(WeirdObjectError) {
+      //If we catch any WeirdObjectError -> return an empty object
+      return '{}';
     }
-    return result + '}';
   }
 
-  //Utility function including the stringify main logic
-  var stringify = function(obj) {
+  //Utility function to stringify an array
+  var stringifyArray = function(arr) {
+    //Using array.join enables to deal with an empty array or a filled in array
+    var arrayString = arr.map(function(value) {
+      return stringifyJSON(value);
+    });
+    return '[' + arrayString.join(',') + ']';      
+  }
 
-    //If a weird object is met, we throw an exception that will be catched by the main program -> unwinding the stack
-    if (typeof obj === 'function' || obj === 'functions' || obj === 'undefined' || obj === undefined) {
-      throw WeirdObjectError;
-    }
-    //Object or Array
-    if (typeof obj === 'object') {
-      //Array
-      if (Array.isArray(obj)) {
-        if (obj.length === 0) {
-          return '[]';
-        } else {
-          return stringifyArray(obj);
-        }
-      //Object
-      } else {
-        if (obj === null) {
-          return 'null';
-        } else if (Object.keys(obj).length === 0) {
-          return '{}';
-        } else {
-          return stringifyObject(obj);
-        }
-      }
-    //No String
-    } else if (typeof obj != 'string') {
-      return "" + obj;
-    //String
+  //NOT VALID JSON:Function, undefined, 'functions' and 'undefined'
+  //If a weird object is met, we throw an WeirdObjectError that will be catched by the stringifyObject method -> unwinding the stack
+  if (typeof obj === 'function' || typeof obj === 'undefined' || obj === 'functions' || obj === undefined) {
+    throw WeirdObjectError;
+  //Object, null, array
+  } else if (typeof obj === 'object') {
+    if (obj === null) {
+      return 'null';
+    } else if (Array.isArray(obj)) {
+      return stringifyArray(obj);
     } else {
-      return "\"" + obj + "\"";
+      return stringifyObject(obj);
     }
-  }
-
-  //The main program launches the stringify function. If WeirdObjectError is intercepted because of a weird object, the catch part will return the default JSON object
-  try {
-    return stringify(obj);
-  } catch (WeirdObjectError) {
-    return '{}';
+  //Not a string
+  } else if (typeof obj != 'string') {
+    return String(obj);      
+  //String
+  } else {
+    return '\"' + obj + '\"';
   }
 
 };
